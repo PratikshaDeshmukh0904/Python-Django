2b1fab92a5ed26944d644388109a430d36bdc0e9
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-09-15 16:08:47 +0530

    Calc-App

--- urls.py
+++ urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-path('',views.home,name='home')
+path('',views.home,name='home'),
+path('registration', views.registration, name="registration"),
+path('adminlogin',views.adminlogin,name="adminlogin"),
+
 ]
--- views.py
+++ views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
+# from .models import  AdminHOD,auth
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html',{'name':'ECS'})
+    return render(request,'home.html',{'name':'ADCET'})
+
+     
+def registration(request):
+    return render(request, 'registration.html')
+     
+def adminlogin(request):
+    return render(request, 'adminlogin.html')
+
+
+# def doLogin(request):
+#     if request.method=='POST':
+#        username = request.POST['username']
+#         password = request.POST['password']
+
+#       user = auth.authenticate(username=='admin' ,password=='admin')
+
+#       if AdminHOD is not None:
+#         auth.doLogin(request,user)
+#         return redirect("/")
+        
+#         else:
+#             messages.info(request,'invalid credentials')
+#             return render(request,'adminlogin.html')
+
+       
